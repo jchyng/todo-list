f7201cdf9e1424924116d71964f4e7b5ef8f736b
정찬영 <jcy0342@gmail.com>
2025-07-31 09:47:12 +0900

    SSR 방식으로 소셜 로그인 구현
    - supabase + google OAuth
    - SSR (middleware)

---
+++ src/app/login/actions.js
@@ -0,0 +1,64 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/lib/supabase/server";
+
+export async function login(formData) {
+  const supabase = await createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
+
+export async function loginWithOAuth(provider) {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo: "http://localhost:3000/auth/callback",
+    },
+  });
+
+  if (data.url) {
+    redirect(data.url); // use the redirect API for your server framework
+  }
+}
+
+export async function googleLogin() {
+  await loginWithOAuth("google");
+}
+
+export async function signup(formData) {
+  const supabase = await createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  const { error } = await supabase.auth.signUp(data);
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
---
+++ src/lib/supabase/middleware.js
@@ -0,0 +1,67 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
+
+export async function updateSession(request) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            request.cookies.set(name, value)
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const path = request.nextUrl.pathname;
+
+  if (user) {
+    const userInfo = {
+      id: user.id,
+      avatar_url: user.user_metadata.avatar_url,
+      email: user.email,
+      full_name: user.user_metadata.full_name,
+    };
+
+    supabaseResponse.cookies.set("user_info", JSON.stringify(userInfo), {
+      httpOnly: false, // 클라이언트에서 접근 가능
+      secure: true,
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 7, // 7일
+    });
+
+    if (path === "/login" || path === "/") return redirect("/todo");
+  } else {
+    supabaseResponse.cookies.delete("user_info");
+
+    if (!path.startsWith("/login")) return redirect("/login");
+  }
+
+  return supabaseResponse;
+}
+
+function redirect(path) {
+  const url = request.nextUrl.clone();
+  url.pathname = path;
+  return NextResponse.redirect(url);
+}
